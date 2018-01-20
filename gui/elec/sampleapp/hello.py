98d5c33ad7fbb1d812dc09075f1b3044ba4b0afb
jmbo <jmbo@jmbo-VirtualBox>
2018-01-21 00:18:51 +0900

    adjusted electron

--- gui/elec/sampleapp/hello.py
+++ gui/elec/sampleapp/hello.py
@@ -3,14 +3,16 @@
 
 from __future__ import print_function
 import time
-from flask import Flask
+from flask import Flask, render_template #追加
 
 app = Flask(__name__)
 
 @app.route("/")
-
 def hello():
-    return "Hello World!<br>This is powered by Python backend."
+    name = "Hoge"
+    #return name
+    return render_template('first.html',template_folder='templates', title='flask test', name=name) #変更
+
 
 if __name__ == "__main__":
     print('on hello')
